28aa955cdbcfd757d12c046ab6a87b5a6bdaecad
Yash Garg <thisisyashgarg@gmail.com>
2023-03-02 18:02:00 +0530

    title changed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { close, logo, menu } from "../assets";
-// import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
