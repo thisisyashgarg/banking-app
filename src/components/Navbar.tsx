c750a2eabe379b8950add75d55b091da6592eb52
Yash Garg <thisisyashgarg@gmail.com>
2023-03-02 17:59:26 +0530

    completed building the website

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { close, logo, menu } from "../assets";
+// import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
@@ -26,6 +27,25 @@ const Navbar = () => {
           className="w-[28px] h-[28px] object-contain"
           onClick={() => setToggle((prev) => !prev)}
         />
+
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex flex-col justify-end items-center flex-1">
+            {navLinks.map((item, index) => (
+              <li
+                className={`font-poppins font-normal cursor-pointer text-[16px] text-white  ${
+                  index === navLinks.length - 1 ? "mr-0" : "mb-4"
+                }`}
+                key={item.id}
+              >
+                <a href={`#${item.id}`}>{item.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </nav>
   );
